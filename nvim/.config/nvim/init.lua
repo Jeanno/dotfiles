8f471867c20744823df0d03f8999fc619c3f0c92
Jeanno <jeanno.cheung@gmail.com>
2025-04-07 12:05:06 -0700

    Update nvim color theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,18 +1,6 @@
 vim.cmd('runtime myinit.vim')
 require("config.lazy")
 require('config/cmp')
- --require('config/refactoring')
-
--- vim.api.nvim_create_autocmd('LspAttach', {
--- 	callback = function(ev)
--- 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
--- 		if client:supports_method('textDocument/completion') then
--- 			vim.lsp.completion.enable(true, client.id, ev.buf, {
--- 				autotrigger = true
--- 			})
--- 		end
--- 	end,
--- })
 
 vim.api.nvim_create_autocmd('TermOpen', {
 	group = vim.api.nvim_create_augroup('custom_termopen', { clear = true }),
